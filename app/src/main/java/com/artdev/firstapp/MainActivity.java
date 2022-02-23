2cc235f44de13e3133bf34a15d735ffd6bf791a5
arthurmdros <arthurmedeiros2005@gmail.com>
2022-02-22 22:01:25 -0300

    Adding onClick listener and changing text elements

--- app/src/main/java/com/artdev/firstapp/MainActivity.java
+++ app/src/main/java/com/artdev/firstapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.artdev.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void prizeDraw(View view){
+        TextView legend = findViewById(R.id.legend);
+        int randomNumber = new Random().nextInt(11);
+        legend.setText("Número sorteado: " + randomNumber);
     }
 }
